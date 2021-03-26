2ea1e29e7af9e1ca2b5db67b85e1d4157e08d197
Rio Capollari <rcapollari@oakland.edu>
2021-03-26 13:27:51 -0400

    edits card

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-	include_once 'header.php';
+include_once 'header.php';
 
 ?>
 
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <style>
-           body {
+        body {
             font-family: "Lato", sans-serif;
         }
 
@@ -59,46 +59,43 @@
 <body>
 
     <div class="sidenav">
-    	<?php
-				if (isset($_SESSION["username"])) {
-					echo "<a style='color: #FFFFFF;'> Welcome ". $_SESSION["username"]. "</a>";
-                    #acctype is the variable for what privilege a user can have 
-                    # 1 is employee 2 is customer 
-                    # the code will print out the account type of the person logged in 
-					#echo "<p> You are ". $_SESSION["acctype"]. "</p>";
-					echo "<a href='home.php'>Home</a>";
-        			echo "<a href=>Open New Account</a>";
-        			echo "<a href=>Start Transaction</a>";
-        			echo "<a href=>Edit Profile</a>";
-					echo "<a href = 'includes/logout-inc.php'>Log out</a>";
-				}
-				else{
-					echo "<a href = 'login.php'>Login</a>";
-					echo "<a href = 'signup.php'>Sign up</a>";
-				}
-		?>
+        <?php
+        if (isset($_SESSION["username"])) {
+            echo "<a style='color: #FFFFFF;'> Welcome " . $_SESSION["username"] . "</a>";
+            #acctype is the variable for what privilege a user can have 
+            # 1 is employee 2 is customer 
+            # the code will print out the account type of the person logged in 
+            #echo "<p> You are ". $_SESSION["acctype"]. "</p>";
+            echo "<a href='home.php'>Home</a>";
+            echo "<a href=>Open New Account</a>";
+            echo "<a href=>Start Transaction</a>";
+            echo "<a href=>Edit Profile</a>";
+            echo "<a href = 'includes/logout-inc.php'>Log out</a>";
+        } else {
+            echo "<a href = 'login.php'>Login</a>";
+            echo "<a href = 'signup.php'>Sign up</a>";
+        }
+        ?>
     </div>
 
     <div class="main">
-      
+
         <div class="w3-container" style="width: 500px">
             <div class="w3-panel w3-card">
-        <?php
-				if (isset($_SESSION["username"])) {
-                echo"<div class= card-body>";
-    
-                   echo " <h4>Checking:</h4>";
-                   echo  "<h4>Savings:</h4>";
-                  }
+                <?php
+                if (isset($_SESSION["username"])) {
+                    echo "<div class= card-body>";
 
-        ?>
-                </div>
+                    echo " <h4>Checking:</h4>";
+                    echo  "<h4>Savings:</h4>";
+                }
+
+                ?>
             </div>
         </div>
-
-
     </div>
 
+    </div>
 
 </body>
 
