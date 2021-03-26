9931d948a83006718fd556eaaa04db19215f46c1
Rio Capollari <rcapollari@oakland.edu>
2021-03-26 13:24:04 -0400

    updates card

--- home.php
+++ home.php
@@ -8,10 +8,8 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
     <style>
            body {
             font-family: "Lato", sans-serif;
@@ -83,8 +81,8 @@
 
     <div class="main">
       
-        <div class="container">
-            <div class="card">
+        <div class="w3-container" style="width: 500px">
+            <div class="w3-panel w3-card">
         <?php
 				if (isset($_SESSION["username"])) {
                 echo"<div class= card-body>";
--- includes/server.php
+++ includes/server.php
@@ -5,7 +5,7 @@ $dbuser = "root";
 $dbpass = "12345";
 $dbname = "bankdb";
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to conenct to '$dbhost");
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Unable to connect to '$dbhost");
 
 if(!$conn){
 	die("Conenction Failed: " . mysqli_connect_error());
